3c7cc438cbae66012c9b9532527fcc3d319cee73
shebing5 <126408626+shebing5@users.noreply.github.com>
2023-04-12 12:49:40 +0800

    Create middleware.js

---
+++ middleware.js
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+
+export function middleware(req) {
+  const credentials = req.headers.get("Authorization");
+  if (!credentials) {
+    return new Response("Access denied", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Secure Area"',
+      },
+    });
+  }
+
+  const [username, password] = Buffer.from(
+    credentials.replace("Basic ", ""),
+    "base64"
+  )
+    .toString()
+    .split(":");
+
+  // Replace these values with your own
+  const USERNAME = process.env.USERNAME;
+  const PASSWORD = process.env.PASSWORD;
+
+  if (username !== USERNAME || password !== PASSWORD) {
+    return new Response("Invalid credentials", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Secure Area"',
+      },
+    });
+  }
+
+  // Only run this middleware for the about/* path
+  if (new URLPattern("/about/*").test(req.nextUrl.pathname)) {
+    return NextResponse.next();
+  }
+}
